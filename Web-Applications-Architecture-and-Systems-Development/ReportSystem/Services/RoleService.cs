e6a499e23aa057cc8dbe2027e19f3ea920b1772c
sergiu nimat <stud.demy@gmail.com>
2020-04-13 12:17:19 +0200

    RoleService && IRoleService interface

---
+++ Web-Applications-Architecture-and-Systems-Development/ReportSystem/Services/RoleService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using ReportSystem.Interfaces;
+using ReportSystem.Models;
+
+namespace ReportSystem.Services
+{
+    public class RoleService : IRoleService
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RoleService(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task CreateRoleIfDoesNotExist(string roleName)
+        {
+            if (!_roleManager.RoleExistsAsync(roleName).Result)
+            {
+                IdentityRole identityRole = new IdentityRole()
+                {
+                    Name = roleName
+                };
+                await _roleManager.CreateAsync(identityRole);
+            }
+        }
+    }
+}
