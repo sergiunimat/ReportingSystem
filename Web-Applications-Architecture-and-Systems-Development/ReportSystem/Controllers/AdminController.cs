d216cdff93444bf9dcde6c6b32792395b1b17f2e
sergiu nimat <stud.demy@gmail.com>
2020-04-19 23:30:49 +0200

    Admin- Users Table

--- Web-Applications-Architecture-and-Systems-Development/ReportSystem/Controllers/AdminController.cs
+++ Web-Applications-Architecture-and-Systems-Development/ReportSystem/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ReportSystem.Models;
 
@@ -11,9 +12,23 @@ namespace ReportSystem.Controllers
     [Authorize(Roles = Role.Administrator)]
     public class AdminController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AdminController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Users()
+        {
+            var userList = _userManager.Users;   
+            return View(userList);
         }
     }
 }
