9a4d643aca199d13bdccde0946951346437fccac
sergiu nimat <stud.demy@gmail.com>
2020-04-19 15:30:36 +0200

    Handle RetrunUrl

--- Web-Applications-Architecture-and-Systems-Development/ReportSystem/Controllers/HomeController.cs
+++ Web-Applications-Architecture-and-Systems-Development/ReportSystem/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ReportSystem.Models;
@@ -13,16 +14,23 @@ namespace ReportSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         
         public IActionResult Index()
         {
-            return View();
+            return User != null && User.Identity.IsAuthenticated
+                ? (IActionResult) RedirectToAction("LogedInIndex", "Home", new {userId = _userManager.GetUserId(User)})
+                : View();
         }
         [Authorize]
         public IActionResult LogedInIndex(string userId)
